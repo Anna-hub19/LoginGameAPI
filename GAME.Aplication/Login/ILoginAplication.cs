e6255d523d95e7cdc65eaa041220ab498b44afa8
Anna Beatriz Bastos <127196700+Anna-hub19@users.noreply.github.com>
2025-02-25 08:23:27 -0300

    Add files via upload

---
+++ GAME.Aplication/Login/ILoginAplication.cs
@@ -0,0 +1,14 @@
+﻿using GAME.Service.Domain.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GAME.Aplication.Login
+{
+    public interface ILoginAplication
+    {
+        public bool GetLogin(PlayerDto playerDto);
+    }
+}
---
+++ GAME.Aplication/Login/LoginAplication.cs
@@ -0,0 +1,23 @@
+﻿using GAME.Service.Domain.Dtos;
+using GAME.Service.Domain.Login;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GAME.Aplication.Login
+{
+    public class LoginAplication : ILoginAplication
+    {
+        private readonly ILoginDomainService _loginDomainService;
+        public LoginAplication(ILoginDomainService loginDomainService)
+        {
+            _loginDomainService = loginDomainService;
+        }
+        public bool GetLogin(PlayerDto playerDto)
+        {
+          return  _loginDomainService.GetLogin(playerDto);
+        }
+    }
+}
