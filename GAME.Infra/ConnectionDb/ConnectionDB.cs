19dfb4f9baee3cb44a8a9a93b51bf6b1cf4ac506
Anna Beatriz Bastos <127196700+Anna-hub19@users.noreply.github.com>
2025-02-25 08:24:44 -0300

    Add files via upload

---
+++ GAME.Infra/ConnectionDb/ConnectionDB.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace GAME.Infra.ConnectionDb
+{
+    public class ConnectionDB
+    {
+        private readonly string _DbConnectionString;
+
+        public ConnectionDB(IConfiguration configuration)
+        {
+            _DbConnectionString = configuration.GetSection("ConnectionStrings").GetSection("PostgresConnection").Value;
+
+        }
+        public string GetString()
+        {
+            return _DbConnectionString;
+        }
+    }
+}
---
+++ GAME.Infra/Repository/Login.cs
@@ -0,0 +1,37 @@
+﻿using GAME.Domain.Entity;
+using GAME.Domain.Repository;
+using GAME.Infra.ConnectionDb;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GAME.Infra.Repository
+{
+    public class Login : ILogin
+    {
+        private ConnectionDB _connectionString;
+        public Login(IConfiguration configuration)
+        {
+            _connectionString = new ConnectionDB(configuration);
+        }
+        bool ILogin.GetLogin(Player player)
+        {
+            using var conn = new NpgsqlConnection(_connectionString.GetString());
+            conn.Open();
+
+            string query = "SELECT COUNT(*) FROM player WHERE nome = @nome AND senha = @senha";
+
+            using var cmd = new NpgsqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("nome", player.Name);
+            cmd.Parameters.AddWithValue("senha", player.Senha); 
+
+            int count = Convert.ToInt32(cmd.ExecuteScalar());
+
+            return count > 0;
+        }
+    }
+}
