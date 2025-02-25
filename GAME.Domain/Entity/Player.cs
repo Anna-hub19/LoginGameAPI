62ccc7b49e479a5a830d19f24b34ba6b73f10f86
Anna Beatriz Bastos <127196700+Anna-hub19@users.noreply.github.com>
2025-02-25 08:24:01 -0300

    Add files via upload

---
+++ GAME.Domain/Entity/Player.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GAME.Domain.Entity
+{
+    public class Player
+    {
+       public string Name { get; set; }
+       public string Senha { get; set; }
+    }
+}
---
+++ GAME.Domain/Repository/ILogin.cs
@@ -0,0 +1,14 @@
+﻿using GAME.Domain.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GAME.Domain.Repository
+{
+    public interface ILogin
+    {
+        public bool GetLogin(Player player);
+    }
+}
